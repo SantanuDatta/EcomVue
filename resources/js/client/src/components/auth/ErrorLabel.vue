674467a0b7358ff35495244b2af04946b5d6370d
SantanuDatta <santanudatta94@gmail.com>
2025-01-25 10:58:05 +0600

    feat(page): added an error label component

---
+++ resources/js/client/src/components/auth/ErrorLabel.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  errors: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
+
+<template>
+  <div v-if="errors?.length" class="space-y-1">
+    <span
+      v-for="(error, index) in errors"
+      :key="index"
+      class="text-red-500 block text-sm font-semibold"
+    >
+      {{ error }}
+    </span>
+  </div>
+</template>
