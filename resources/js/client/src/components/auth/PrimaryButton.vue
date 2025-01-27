74829b9ae0c263c53d6c34667047c249f5436de4
SantanuDatta <santanudatta94@gmail.com>
2025-01-27 11:38:44 +0600

    fix(deps): bump tailwind version to 4.0

--- resources/js/client/src/components/auth/PrimaryButton.vue
+++ resources/js/client/src/components/auth/PrimaryButton.vue
@@ -18,7 +18,7 @@ defineProps({
     :processing="processing"
     :class="{ 'opacity-80': processing }"
     :disabled="processing"
-    class="flex w-full justify-center items-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    class="flex w-full justify-center items-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-xs hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
   >
     <svg
       v-if="processing"
--- resources/js/client/src/layouts/AppLayout.vue
+++ resources/js/client/src/layouts/AppLayout.vue
@@ -215,7 +215,7 @@ const sidebarOpen = ref(false);
 
     <div class="lg:pl-72">
       <div
-        class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8"
+        class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-xs sm:gap-x-6 sm:px-6 lg:px-8"
       >
         <button
           type="button"
@@ -288,7 +288,7 @@ const sidebarOpen = ref(false);
                 leave-to-class="transform opacity-0 scale-95"
               >
                 <MenuItems
-                  class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"
+                  class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-hidden"
                 >
                   <MenuItem
                     v-for="item in userNavigation"
