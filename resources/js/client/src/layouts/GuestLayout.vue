7164337a925bc2ce0cbde73da87c0226678e91e2
SantanuDatta <santanudatta94@gmail.com>
2025-02-09 11:27:02 +0600

    fix: image link fix

--- resources/js/client/src/layouts/GuestLayout.vue
+++ resources/js/client/src/layouts/GuestLayout.vue
@@ -12,7 +12,6 @@ const { title } = defineProps({
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <ApplicationLogo
         class="mx-auto h-10 w-auto"
-        src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
         alt="Your Company"
       />
       <h2
