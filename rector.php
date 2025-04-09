6202ebb8454e9f0bd9965ef6c1e0e34476c6c6a1
SantanuDatta <santanudatta94@gmail.com>
2025-04-09 13:04:26 +0600

    chore: added laravel rules & compat with phpstan

--- rector.php
+++ rector.php
@@ -2,7 +2,19 @@
 
 declare(strict_types=1);
 
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
 use Rector\Config\RectorConfig;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use Rector\Set\ValueObject\SetList;
+use Rector\ValueObject\PhpVersion;
+use RectorLaravel\Rector\Class_\AddExtendsAnnotationToModelFactoriesRector;
+use RectorLaravel\Rector\ClassMethod\AddGenericReturnTypeToRelationsRector;
+use RectorLaravel\Rector\MethodCall\EloquentWhereRelationTypeHintingParameterRector;
+use RectorLaravel\Rector\MethodCall\EloquentWhereTypeHintClosureParameterRector;
+use RectorLaravel\Rector\MethodCall\ValidationRuleArrayStringValueToArrayRector;
+use RectorLaravel\Rector\MethodCall\WhereToWhereLikeRector;
+use RectorLaravel\Rector\PropertyFetch\ReplaceFakerInstanceWithHelperRector;
+use RectorLaravel\Set\LaravelSetList;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -10,16 +22,36 @@ return RectorConfig::configure()
         __DIR__.'/bootstrap/app.php',
         __DIR__.'/database',
         __DIR__.'/public',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
     ])
-    // ->withSkip([
-    //     AddOverrideAttributeToOverriddenMethodsRector::class,
-    // ]) //also this one with you will understand why use this or not
-    ->withPreparedSets(
-        deadCode: true,
-        codeQuality: true,
-        typeDeclarations: true,
-        privatization: true,
-        earlyReturn: true,
-        strictBooleans: true,
-    )
+    ->withBootstrapFiles([__DIR__.'/vendor/larastan/larastan/bootstrap.php'])
+    ->withPHPStanConfigs([__DIR__.'/phpstan.neon'])
+    ->withSkip([
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+        EncapsedStringsToSprintfRector::class,
+    ]) // also this one which you will understand later why use this or not
+    ->withSets([
+        SetList::DEAD_CODE,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::TYPE_DECLARATION,
+        SetList::PRIVATIZATION,
+        SetList::EARLY_RETURN,
+        SetList::STRICT_BOOLEANS,
+        LaravelSetList::LARAVEL_110,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+    ])
+    ->withRules([
+        AddGenericReturnTypeToRelationsRector::class,
+        AddExtendsAnnotationToModelFactoriesRector::class,
+        EloquentWhereRelationTypeHintingParameterRector::class,
+        EloquentWhereTypeHintClosureParameterRector::class,
+        ReplaceFakerInstanceWithHelperRector::class,
+        ValidationRuleArrayStringValueToArrayRector::class,
+        WhereToWhereLikeRector::class,
+    ])
+    ->withImportNames()
+    ->withPhpVersion(PhpVersion::PHP_84)
     ->withPhpSets();
