f8c5cb3eee159eb4cca862ffb4ebb1f51f498be9
SantanuDatta <santanudatta94@gmail.com>
2025-04-13 11:18:33 +0600

    fix: linting issue fix

--- database/migrations/2025_01_20_052758_create_cart_items_table.php
+++ database/migrations/2025_01_20_052758_create_cart_items_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('cart_items', function (Blueprint $table): void {
             $table->id();
-            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');;
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');
             $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
             $table->integer('quantity');
             $table->timestamps();
