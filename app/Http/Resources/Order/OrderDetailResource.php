e56334158ab1bb735712af791ea1360ff3bc7db0
SantanuDatta <santanudatta94@gmail.com>
2025-04-20 10:39:57 +0600

    feat: fill orderdetail and orderitem resources

--- app/Http/Resources/Order/OrderDetailResource.php
+++ app/Http/Resources/Order/OrderDetailResource.php
@@ -7,6 +7,20 @@ namespace App\Http\Resources\Order;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property-read int $id
+ * @property-read int $order_id
+ * @property-read string $first_name
+ * @property-read string $last_name
+ * @property-read string $email
+ * @property-read string|null $phone
+ * @property-read string $address1
+ * @property-read string|null $address2
+ * @property-read string $city
+ * @property-read string|null $state
+ * @property-read string $zip_code
+ * @property-read string $country_code
+ */
 class OrderDetailResource extends JsonResource
 {
     /**
@@ -16,6 +30,19 @@ class OrderDetailResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'order_id' => $this->order_id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'address1' => $this->address1,
+            'address2' => $this->address2,
+            'city' => $this->city,
+            'state' => $this->state,
+            'zip_code' => $this->zip_code,
+            'country_code' => $this->country_code,
+        ];
     }
 }
--- app/Http/Resources/Order/OrderItemResource.php
+++ app/Http/Resources/Order/OrderItemResource.php
@@ -4,9 +4,18 @@ declare(strict_types=1);
 
 namespace App\Http\Resources\Order;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property-read int $id
+ * @property-read int $order_id
+ * @property-read int $product_id
+ * @property-read int $quantity
+ * @property-read int $unit_price
+ * @property-read Product $product
+ */
 class OrderItemResource extends JsonResource
 {
     /**
@@ -16,6 +25,12 @@ class OrderItemResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'order_id' => $this->order_id,
+            'name' => $this->product->title,
+            'quantity' => $this->quantity,
+            'unit_price' => $this->unit_price,
+        ];
     }
 }
